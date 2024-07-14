6721a53f550b232cd5a12424532d4fbdba1bb642
Alex Hewitt <alexhewitt.j@gmail.com>
2024-07-14 11:56:00 -0500

    add in functionality for get urls from html

--- crawl.js
+++ crawl.js
@@ -12,15 +12,19 @@ function normalizeURL(url) {
 function getURLsFromHTML(htmlBody, baseURL) {
   const dom = new JSDOM(htmlBody);
   const links = dom.window.document.querySelectorAll("a");
-  const urls = []
+  const urls = [];
   for (let link of links) {
-    console.log(link.href + link.hostname);
-    if (link.hostname === baseURL) {
-      urls.push(link.href)
-    } else {
-      urls.push(`${link.hostname}`)
+    if (link.hasAttribute("href")) {
+      let href = link.getAttribute("href");
+      try {
+        href = new URL(href, baseURL).href;
+        urls.push(href);
+      } catch (err) {
+        console.log(`${err.message}: ${href}`);
+      }
     }
   }
+  return urls;
 }
 getURLsFromHTML(
   '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>',
