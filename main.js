dcde4a8280d4ab50aa540dd692ab8cfded59f0db
Alex <alexhewitt.j@gmail.com>
2024-07-15 11:49:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-console.log("hello world");
-
-open;
+import readline from "node:readline";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("hello?", (name) => {
+  console.log(name);
+  rl.close;
+});
